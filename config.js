3525a99d7dc83ce98fee2921af850c3710a5dc83
Phạm Thế Sơn <94886989+sondeptraicute@users.noreply.github.com>
2022-04-05 21:01:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'He luu cậuu!',
+    introDesc: `tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.`,
+    btnIntro: 'Cậu yêu tớ phải không nào <3',
+    title: 'Nếu cậu không trả lời mà thoát ra tức là muốn làm vơ tớ rùi đó nhaaa :v',
+   
+    btnYes: 'Yêu cậuu<3',
+    btnNo: 'Không, Cậu mơ à :>>',
+    question:'Nói cho tớ lí do cậu thích tớ đi :vvv',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'vì cậu đẹp trai vler <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu 😘😘',
+    messDesc: 'Tối nay tớ qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii  <3',
+    messLink: 'http://fb.com/m.me/SonSadBoiz.CC' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
